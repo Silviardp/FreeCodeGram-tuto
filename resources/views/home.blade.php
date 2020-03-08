bd08f7b29d778fcf2540e877dd75ea8bddc4d899
Silvia Rocha <silviadprocha@gmail.com>
2020-03-08 15:04:47 +0900

    continue with home page html and CSS desription + pictures

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,16 +6,30 @@
       <div class="col-3 p-5">
          <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_ohc=EAD_lLwp4MgAX_R8l5y&oh=2f6b2c2cd55d8afa9db762f8a7070b18&oe=5E9A00BA" alt="" class="rounded-circle">
       </div>
-      <div class="col-9 pt-5">
-      <div><h1>freecodecamp</h1></div>
-      <div class="d-flex">
-        <div class="pr-5"><strong>153</strong> posts</div>
-        <div class="pr-5"><strong>23k</strong> followers</div>
-        <div class="pr-5"><strong>23k</strong> following</div>
-      </div>
-
+        <div class="col-9 pt-5">
+        <div><h1>freecodecamp</h1></div>
+        <div class="d-flex">
+          <div class="pr-5"><strong>273</strong> posts</div>
+          <div class="pr-5"><strong>39.7k</strong> followers</div>
+          <div class="pr-5"><strong>235</strong> following</div>
+        </div>
+        <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+        <div class="">We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+        <div class=""><a href="www.freecodecamp.org" class="">www.freecodecamp.org</a></div>
       </div>
   </div>
 
+  <div class="row pt-5">
+    <div class="col-4">
+      <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/88374782_543733416501650_6638052850750298038_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=102&_nc_ohc=IypSYzxP4-gAX-KxWl9&oh=83ce55b4f883c66b4529829353c6ec6e&oe=5E8D8F49" alt="" class="w-100">
+    </div>
+       <div class="col-4">
+      <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.113.931.931a/s640x640/84217667_548898739065440_6328985649004661540_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_cat=108&_nc_ohc=U2Mk7jOzauAAX9gTcDe&oh=0e6f9dcbf9aa7da4885f0ba80aa934ca&oe=5E958E5E" alt="" class="w-100">
+    </div>
+       <div class="col-4">
+      <img src="https://scontent-frt3-2.cdninstagram.com/v/t51.2885-15/e35/c100.0.501.501a/89260743_2658694764411607_948672185790002051_n.jpg?_nc_ht=scontent-frt3-2.cdninstagram.com&_nc_cat=107&_nc_ohc=07PeNzM6VO4AX9JGoJH&oh=6f214d94b978f4fdbd5ff0710190ceed&oe=5E982F5D" alt="" class="w-100">
+    </div>
+
+  </div>
 </div>
 @endsection
