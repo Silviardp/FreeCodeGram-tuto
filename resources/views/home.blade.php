af7527d017c7b104b51771b1e070b80be72546e0
Silvia Rocha <silviadprocha@gmail.com>
2020-03-08 14:52:39 +0900

    change font family to open sans and also bg color to white + start changing html + css home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,20 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+  <div class="row">
+      <div class="col-3 p-5">
+         <img src="https://scontent-frt3-1.cdninstagram.com/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com&_nc_ohc=EAD_lLwp4MgAX_R8l5y&oh=2f6b2c2cd55d8afa9db762f8a7070b18&oe=5E9A00BA" alt="" class="rounded-circle">
+      </div>
+      <div class="col-9 pt-5">
+      <div><h1>freecodecamp</h1></div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>153</strong> posts</div>
+        <div class="pr-5"><strong>23k</strong> followers</div>
+        <div class="pr-5"><strong>23k</strong> following</div>
+      </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+      </div>
+  </div>
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
