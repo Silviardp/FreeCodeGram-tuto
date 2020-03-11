0f34f29aab90cff76f3e7a4a2b62aa25bd794ec4
Silvia Rocha <silviadprocha@gmail.com>
2020-03-11 12:31:24 +0900

    add a public function profileImage

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,12 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage()
+    {
+      $imagePath = ($this->image) ? $this->image : 'profile/JNiNHZYPax0bk1mZWBDuZbvKfghk7OsZRJjsTrXO.png';
+      return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
       return $this->belongsTo(User::class);
